5590c8692a35246115cb3a7feae5911231dd8acf
vsreyes <virgilio.s.reyes@gmail.com>
2020-10-27 15:47:01 +0100

    Add Experience

---
+++ client/src/components/profile-forms/AddExperience.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addExperience } from '../../actions/profile';
+
+const AddExperience = props => {
+  return <div></div>;
+};
+
+AddExperience.propTypes = {
+  addExperience: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addExperience })(AddExperience);
