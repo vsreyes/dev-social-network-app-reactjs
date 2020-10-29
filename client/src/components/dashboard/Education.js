4f0933c4b96d1cb4371b25335e66777a0997f211
vsreyes <virgilio.s.reyes@gmail.com>
2020-10-29 18:39:33 +0100

    delete profile

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,8 +2,9 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { deleteEducation } from '../../actions/profile';
 
-const Education = ({ education }) => {
+const Education = ({ education, deleteEducation }) => {
   const educations = education.map(edu => (
     <tr key={edu._id}>
       <td>{edu.school}</td>
@@ -16,7 +17,14 @@ const Education = ({ education }) => {
           <Moment format='YYYY/MM/DD'>{edu.to}</Moment>
         )}
       </td>
-      <button className='btn btn-danger'>Delete</button>
+      <td>
+        <button
+          onClick={() => deleteEducation(edu._id)}
+          className='btn btn-danger'
+        >
+          Delete
+        </button>
+      </td>
     </tr>
   ));
 
@@ -40,6 +48,7 @@ const Education = ({ education }) => {
 
 Education.propTypes = {
   education: PropTypes.array.isRequired,
+  deleteEducation: PropTypes.func.isRequired,
 };
 
-export default Education;
+export default connect(null, { deleteEducation })(Education);
