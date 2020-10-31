08c46f0ed5f8f6d2a3c40226a6fa4403370bfa15
vsreyes <virgilio.s.reyes@gmail.com>
2020-10-31 18:40:05 +0100

    Profile.js

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,19 @@
+import React, { Fragment, useEffect } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getProfileById } from '../../actions/profile';
+
+const Profile = props => {
+    return (
+        <div>
+            Profile
+        </div>
+    )
+}
+
+Profile.propTypes = {
+
+}
+
+export default Profile
