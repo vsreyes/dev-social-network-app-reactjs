ec6ad99f6c9eca3269c49ee6ba058c8a4ec9cf1d
vsreyes <virgilio.s.reyes@gmail.com>
2020-11-07 15:11:12 +0100

    Adding Posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -9,7 +9,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     getPosts();
   }, [getPosts]);
 
-  return <div></div>;
+  return loading ? <Spinner /> : <Fragment>Test</Fragment>;
 };
 
 Posts.propTypes = {
